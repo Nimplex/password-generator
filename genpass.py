89855db20a9a49ffe2c7f8cc2c21ab80837a10f3
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-31 21:28:03 +0100

    Better password generation.

--- genpass.py
+++ genpass.py
@@ -1,12 +1,13 @@
 import random
 import pyperclip
-c=list("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-=[]\\{|}~`")
-p=""
-l=int(input("Length: "))
-i=0
-while i<l:
- p+=c[random.randint(0,len(c)-1)]
- i+=1
-print("Password: "+p)
-if(input("Copy to clipboard (Y/N)? "))in["Y","y"]:
- pyperclip.copy(p)
+
+password = ""
+length = int(input("Length: "))
+
+for i in range(0, length):
+    password += chr(random.randint(97, 123))
+
+print("Password: " + password)
+
+if (input("Copy to clipboard (Y/N)? ")) in["Y","y"]:
+    pyperclip.copy(password)
